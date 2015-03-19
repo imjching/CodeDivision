c6a9ba635deac61a83bde0c88aa7c3c0568f68c4
imjching <imjching@hotmail.com>
2015-03-19 15:18:33 +0000

    Modified files to support rspec. Added validations to fields in  table

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,30 @@ require_relative '../../db/config'
 class Student < ActiveRecord::Base
     has_many :students_teachers
     has_many :teachers, through: :students_teachers
+
+    validates :email, uniqueness: true,
+                      format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/ }
+    validates :age, numericality: { greater_than_or_equal_to: 5 }
+    validate :valid_phone?
+
+    def valid_phone?
+        if self.phone.scan(/\d+/).join.length < 10
+            errors.add(:phone, "must contain at least 10 digits, excluding non-numeric characters")
+        end
+    end
+
+    def name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    def name=(value)
+        name = value.split(" ")
+        self.first_name = name[0]
+        self.last_name = name[1]
+    end
+
+    def age
+        now = Date.today
+        age = now.year - self.birthday.year - ((now.month > self.birthday.month || (now.month == self.birthday.month && now.day >= self.birthday.day)) ? 0 : 1)
+    end
 end
--- lib/students_importer.rb
+++ lib/students_importer.rb
@@ -1,3 +1,4 @@
+require 'date'
 require_relative '../app/models/student'
 require_relative '../app/models/teacher'
 require_relative '../app/models/students_teacher'
@@ -7,13 +8,12 @@ module StudentsImporter
         field_names = nil
         Student.transaction do
             File.open(filename).each do |line|
-                line.gsub!(",male,", ",1,")
-                line.gsub!(",female,", ",0,")
                 data = line.chomp.split(',')
                 if field_names.nil?
                     field_names = data
                 else
                     attribute_hash = Hash[field_names.zip(data)]
+                    attribute_hash["birthday"] = Date.parse(attribute_hash["birthday"])
                     student = Student.create!(attribute_hash)
                 end
             end
