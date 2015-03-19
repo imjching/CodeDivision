9973e629c8a6a17e6de397b98f64957c0fe7791f
imjching <imjching@hotmail.com>
2015-03-19 10:43:12 +0000

    Some comments about foreign keys

--- db/migrate/20150319113033_create_tweets.rb
+++ db/migrate/20150319113033_create_tweets.rb
@@ -5,7 +5,8 @@ class CreateTweets < ActiveRecord::Migration
         create_table :tweets do |t|
             t.string :tweet_id
             t.string :tweet_text, :limit => 140
-            t.integer :congress_member_id
+            t.integer :congress_member_id, index: true
+            # Same as "t.belongs_to :congress_member, index: true"
             t.timestamps null: false
         end
     end
