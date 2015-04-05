78872785a90220ba7386469487ec912e8fe65a84
imjching <imjching@hotmail.com>
2015-04-05 11:37:23 +0800

    Each album has many photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,20 @@
 class Photo < ActiveRecord::Base
-  # Remember to create a migration!
+  mount_uploader :photo_path, PhotoUploader
+  belongs_to :album
+
+  validates :photo_path, presence: true
+  validates :album_id, presence: true, uniqueness: { scope: :photo_path }
+
+  before_destroy :remember_image
+  after_destroy :remove_img
+
+  protected
+  def remember_image
+    @image_name = self[:photo_path]
+  end
+
+  def remove_img
+    File.delete("#{APP_ROOT.join('public', 'uploads')}/#{@image_name}")
+    File.delete("#{APP_ROOT.join('public', 'uploads')}/thumb_#{@image_name}")
+  end
 end
--- db/migrate/20150402092434_create_photos.rb
+++ db/migrate/20150402092434_create_photos.rb
@@ -1,4 +1,11 @@
 class CreatePhotos < ActiveRecord::Migration
   def change
+    create_table :photos do |t|
+      t.string :photo_path
+      t.string :caption
+      t.references :album
+      t.timestamps null: false
+    end
   end
 end
+#TODO: unique id SecureRandom.uuid for unique id instead of photo_id
