2f2cbd0421467c6d69cda18a933231cdf556fefb
imjching <imjching@hotmail.com>
2015-04-05 11:37:03 +0800

    Each users have one album

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,15 @@
 class Album < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to :user
+  has_many :photos, dependent: :destroy
+
+  validates :name, presence: true
+  validates :user_id, presence: true
+
+  def random_thumb
+    if photos.empty?
+      "http://placehold.it/250x250"
+    else
+      photos.sample.photo_path.thumb.url
+    end
+  end
 end
--- db/migrate/20150402092402_create_albums.rb
+++ db/migrate/20150402092402_create_albums.rb
@@ -1,4 +1,10 @@
 class CreateAlbums < ActiveRecord::Migration
   def change
+    create_table :albums do |t|
+      t.string :name
+      t.references :user
+      t.timestamps null: false
+    end
   end
 end
+#TODO: slug for albums
